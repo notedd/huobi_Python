4898ce1491d25878beff0c2417637c18e86ffced
zhibing.tian <zhibing.tian@qunar.com>
2021-03-30 11:05:32 +0800

    分析数据

--- example/tushare/tools.py
+++ example/tushare/tools.py
@@ -20,10 +20,12 @@ def get_db():
                            database=constant.g_database)
 
 
+# 股票基本信息入库 每次清空后重新载入最新
 def produce_stock_base_info():
     pro = get_ts().pro_api()
-    data = pro.stock_basic()
-    print(data)
+    data = pro.stock_basic(
+        fields='ts_code,symbol,name,area,industry,fullname,enname,market,exchange,curr_type,list_status,list_date,delist_date,is_hs')
+    # print(data)
     db = get_db()
     cursor = db.cursor()
     cursor.execute("truncate table stock_base_info")
@@ -32,7 +34,22 @@ def produce_stock_base_info():
     data.to_sql('stock_base_info', con=conn, if_exists='append', index=False)
 
 
-def produce_stock_info(freq, start_date, end_date, ts_codes):
+# 查询所有股票基本信息 全量查询
+def get_all_stock_base_info():
+    db = get_db()
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+    # 使用 execute()  方法执行 SQL 查询
+    cursor.execute(
+        "select ts_code,symbol,name,area,industry,fullname,market,exchange,curr_type,list_status,list_date,delist_date,is_hs,update_time from stock_base_info ")
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchall()
+    db.close()
+    return data
+
+
+# 导入数据库数据 有长度限制
+def produce_kline_info(freq, start_date, end_date, ts_codes):
     if freq == 'D':
         table = "stock_daily"
     elif freq == 'W':
@@ -42,59 +59,58 @@ def produce_stock_info(freq, start_date, end_date, ts_codes):
     else:
         return
     print(table)
-    df = get_ts().pro_bar(ts_code=ts_codes, freq=freq, start_date=start_date, end_date=end_date)
+
+    ts_codes_tushare = ""
+    ts_codes_db = ""
+
+    for tscode in ts_codes:
+        ts_codes_tushare = ts_codes_tushare + "," + tscode
+        ts_codes_db = ts_codes_db + "'" + tscode + "',"
+
+    print(ts_codes_tushare[1:])
+    print(ts_codes_db[0:-1])
+
+    # 查询数据
+    df = get_ts().pro_bar(ts_code=ts_codes_tushare[1:], freq=freq, start_date=start_date, end_date=end_date)
+
     print(df)
+
+    # 先删除已有的数据
+    db = get_db()
+    cursor = db.cursor()
+    sqlo = "delete from {}  where trade_date between '{}' and '{}' and ts_code in ({})"
+    sql = sqlo.format(table, start_date, end_date, ts_codes_db[0:-1])
+    #print(sql)
+    cursor.execute(sql)
+    db.commit()
+
+    # 导入数据
     conn = get_conn()
     df.to_sql(table, con=conn, if_exists='append', index=False)
 
 
-def get_all_stock_base_info():
-    db = get_db()
-    # 使用 cursor() 方法创建一个游标对象 cursor
-    cursor = db.cursor()
-    # 使用 execute()  方法执行 SQL 查询
-    count = cursor.execute("select ts_code from stock_base_info ")
-    # 使用 fetchone() 方法获取单条数据.
-    data = cursor.fetchall()
-    db.close()
-    return data
-
-
-def product_daily_info(start_date, end_date):
+def product_stock_kline_info(freq, start_date, end_date,size):
     data = get_all_stock_base_info()
-    ts_code = ""
+    ts_codes = []
     for obj in data:
-        ts_code = ts_code + "," + obj[0]
-    print(ts_code)
+        ts_codes.append(obj[0])
 
-    db = get_db()
-    cursor = db.cursor()
-    sqlo = "delete from stock_daily  where trade_date between '{}' and '{}'"
-    sql = sqlo.format(start_date, end_date)
-    cursor.execute(sql)
-    db.commit()
-
-    produce_stock_info(freq='D', start_date=start_date, end_date=end_date, ts_codes=ts_code)
+    for ts_codes_temp in partition(ts_codes, size):
+        print("#####################################################################################")
+        produce_kline_info(freq=freq, start_date=start_date, end_date=end_date, ts_codes=ts_codes_temp)
 
 
-def product_monthly_info(start_date, end_date):
-    data = get_all_stock_base_info()
-    ts_code = ""
-    count = 0
-    for obj in data:
-        count = count + 1
-        ts_code = ts_code + "," + obj[0]
-
-    db = get_db()
-    cursor = db.cursor()
-    sqlo = "delete from stock_monthly  where trade_date between '{}' and '{}'"
-    sql = sqlo.format(start_date, end_date)
-    cursor.execute(sql)
-    db.commit()
-
-    produce_stock_info(freq='M', start_date=start_date, end_date=end_date, ts_codes=ts_code)
+def partition(ls, size):
+    """
+    Returns a new list with elements
+    of which is a list of certain size.
+        >>> partition([1, 2, 3, 4], 3)
+        [[1, 2, 3], [4]]
+    """
+    return [ls[i:i + size] for i in range(0, len(ls), size)]
 
 
 # produce_stock_base_info()
-# produce_stock_info(freq='M', start_date='20210201', end_date='20210331', ts_codes='000004.SZ')
-# product_monthly_info("20210101", "20210301")
+# produce_kline_info(freq='D', start_date='20210326', end_date='20210329', ts_codes=['000001.SZ','000004.SZ'])
+# product_stock_kline_info('D', "20210326", "20210329",50)
+# product_stock_kline_info('M', "20210101", "20210329",1)
